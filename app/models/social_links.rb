25a371fe407e24911c0110f0b40eaebd90a4e454
Yechiel Kalmenson <contact@yechiel.me>
2018-02-10 21:13:20 -0500

    create a SocialLinks class with links to social links

---
+++ app/models/social_links.rb
@@ -0,0 +1,27 @@
+class SocialLinks
+    
+    def email
+        "contact@yechiel.me"
+    end
+
+    def twitter
+        "https://twitter.com/yechielk"
+    end
+
+    def linkedin
+        "https://www.linkedin.com/in/yechiel-k"
+    end
+
+    def quora
+        "https://www.quora.com/profile/Yechiel-Kay"
+    end
+
+    def blog
+        "https://blog.yechiel.me/"
+    end
+
+    def github
+        "https://github.com/achasveachas"
+    end
+
+end
