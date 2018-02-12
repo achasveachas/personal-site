dc56dedf37480ed1247e43c4d178b96e46c42084
Yechiel Kalmenson <contact@yechiel.me>
2018-02-11 20:22:25 -0500

    set up the create projects logic in the controller

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -3,17 +3,31 @@ class Admin::ProjectsController < ApplicationController
   before_action :require_admin
 
   def new
+    @project = Project.new
   end
 
   def edit
+    @project = Preject.find_by(id: params[:id])
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def delete
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :demo_url, :demo_username, :demo_password, :repo, :blog_post)
   end
 end
