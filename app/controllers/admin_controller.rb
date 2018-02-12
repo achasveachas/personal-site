0c5339dcc30d60010358247a3ebe662d1f40342f
Yechiel Kalmenson <contact@yechiel.me>
2018-02-11 19:42:49 -0500

    create login controllers and logic

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,24 @@
 class AdminController < ApplicationController
     
     def login
-        
+
     end
 
     def new_session
-        
+        if params[:password] == "test"
+            session[:admin] = true 
+            redirect_to projects_path
+        else
+            render :login
+        end
     end
 
     def admin
 
+    end
+
+    def logout
+        session[:admin] = false
+        redirect_to root_path
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :get_page_title
+  helper_method :get_page_title, :require_admin
 
   private
 
   def get_page_title
     params[:action].capitalize
+  end
+
+  def require_admin
+    redirect_to login_path unless session[:admin]
   end
 end
