65ad8db0f1984b302781234e6fe4ab56453e67f0
Yechiel Kalmenson <contact@yechiel.me>
2018-02-11 20:38:13 -0500

    fix typo

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -7,7 +7,7 @@ class Admin::ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Preject.find_by(id: params[:id])
+    @project = Project.find_by(id: params[:id])
   end
 
   def create
@@ -20,6 +20,12 @@ class Admin::ProjectsController < ApplicationController
   end
 
   def update
+    @project = Project.find_by(id: params[:id])
+    if @project.update_attributes(project_params)
+      redirect_to projects_path
+    else
+      render :edit
+    end
   end
 
   def delete
