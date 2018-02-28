5029c6d0891b24adc72f061751d1140f72ad9e06
Yechiel Kalmenson <contact@yechiel.me>
2018-02-27 22:38:51 -0500

    add session logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,12 @@ class ApplicationController < ActionController::Base
   end
 
   def log_session
-    SessionLog.create(company: params[:company]) if params[:company]
+    log = SessionLog.new
+    log.company = params[:company] if params[:company]
+    data = request.location.data
+    location = [data['city'], data['state'], data['zipcode'], data['country_name']].select{|i| i && !i.empty?}.join(', ')
+    log.location = location
+    log.ip = data['ip']
+    log.save
   end
 end
