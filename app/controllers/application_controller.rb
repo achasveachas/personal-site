4dcd479fe1e2115fe8757681b4f852100f369673
Yechiel Kalmenson <contact@yechiel.me>
2018-03-03 22:08:42 -0500

    add page visited logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,7 @@ class ApplicationController < ActionController::Base
       location = [data['city'], data['state'], data['zipcode'], data['country_name']].select{|i| i && !i.empty?}.join(', ')
       log.location = location
       log.ip = data['ip']
+      log.page_visited = params[:action]
       log.save
     end
   end
