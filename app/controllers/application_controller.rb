8246f41ccb45821623a4cdc6a46ca966df8263c6
Yechiel Kalmenson <contact@yechiel.me>
2018-03-18 12:37:21 -0400

    remove old log_session method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,18 +17,6 @@ class ApplicationController < ActionController::Base
     session[:admin]
   end
 
-  def log_session
-    data = request.location.data
-    if !is_admin? && !SessionLog.whitelist.include?(data['ip'])
-      log = SessionLog.new
-      log.company = params[:company] if params[:company]
-      location = [data['city'], data['state'], data['zipcode'], data['country_name']].select{|i| i && !i.empty?}.join(', ')
-      log.location = location
-      log.ip = data['ip']
-      log.page_visited = params[:action]
-      log.save
-    end
-  end
 
   def track_action
     ahoy.track "Ran action", params
