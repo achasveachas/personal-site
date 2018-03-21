2f151f668e71a77e4ca748a4735f3b20aa8750cc
Yechiel Kalmenson <contact@yechiel.me>
2018-03-20 20:41:28 -0400

    hope this fixes things

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,9 +1,8 @@
 class Ahoy::Store < Ahoy::DatabaseStore
-    def track_visit(data)
-        binding.pry
-        data[:ip] = request.env['HTTP_CF_CONNECTING_IP'] || request.remote_ip
-        super(data)
-    end
+    # def track_visit(data)
+    #     data[:ip] = request.env['HTTP_CF_CONNECTING_IP'] || request.remote_ip
+    #     super(data)
+    # end
 end
 
 # set to true for JavaScript tracking
