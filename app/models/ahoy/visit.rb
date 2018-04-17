9017a7fb95e8bb75c0481de6f91a3b72032e9c30
Yechiel Kalmenson <contact@yechiel.me>
2018-04-16 20:32:17 -0400

    add nxt and previous methods to visit class

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -8,11 +8,11 @@ class Ahoy::Visit < ApplicationRecord
   end
 
   def next
-    self.class.find_by(id: self.id + 1)
+    self.class.where('id > ?', self.id).order('id asc').first
   end
 
   def previous
-    self.class.find_by(id: self.id - 1)
+    self.class.where('id < ?', self.id).order('id asc').last
   end
 
 end
