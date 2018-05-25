d92517dffbb59b9cdd97759419d7937a5cdebba0
Yechiel Kalmenson <contact@yechiel.me>
2018-05-24 21:22:25 -0400

    add line about the magic page to the console

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,4 @@
-console.log("Thanks for stopping by!\nTo get in touch:\nWebsite: yechiel.me\nEmail: holler@yechiel.me\nBlog: blog.yechiel.me\nTwitter: @yechielk\nGitHub: achasveachas\nLinkdIn: www.linkedin.com/in/yechiel-k")
+console.log("Thanks for stopping by!\nTo get in touch:\nWebsite: www.yechiel.me\nEmail: holler@yechiel.me\nBlog: blog.yechiel.me\nTwitter: @yechielk\nGitHub: achasveachas\nLinkdIn: www.linkedin.com/in/yechiel-k\n\n\nStop by https://yechiel.me/magic for some cool stuff")
 
 if (window.location.search) {
     const company = decodeURI(window.location.search.split("=")[1])
