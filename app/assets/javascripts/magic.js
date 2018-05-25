85d1a4469d4e5717082e68d5767b91dffb7b9626
Yechiel Kalmenson <contact@yechiel.me>
2018-05-24 21:19:46 -0400

    add the logic

--- app/assets/javascripts/magic.js
+++ app/assets/javascripts/magic.js
@@ -1,9 +1,27 @@
 function calculate() {
     var errorMessage = invalidInputs();
+
     if(errorMessage){
-        document.getElementById('result').innerHTML = errorMessage
+        document.getElementById('result').innerHTML = errorMessage;
         return
     }
+    // Get the original values of x3, x5, and x7
+    var x3 = parseInt(document.getElementById('x3').value);
+    var x5 = parseInt(document.getElementById('x5').value);
+    var x7 = parseInt(document.getElementById('x7').value);
+    
+    // Multiply the Xs by the appropriate numbers
+    x3 *= 70;
+    x5 *= 21;
+    x7 *= 15;
+
+    // Add them up and substract 105 until you get the number
+    var total = x3 + x5 + x7;
+    while (total >= 105) {
+        total -= 105
+    }
+
+    document.getElementById('result').innerHTML = "Your number was: <strong>" + total + "</strong>"
 }
 
 function invalidInputs() {
@@ -14,6 +32,7 @@ function invalidInputs() {
             errors.push(inputs[i].dataset["x"]);
         }
     }
+
     if (errors.length > 0) {   
         return "The values for the following fields seem off: " + errors.join(", ") + ".<br>Please check your math."
     }
