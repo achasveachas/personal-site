b03e6b1779991da5ff42ba12628bdd3e58675636
Yechiel Kalmenson <contact@yechiel.me>
2018-05-24 20:50:34 -0400

    add validation on the input

--- app/assets/javascripts/magic.js
+++ app/assets/javascripts/magic.js
@@ -1,3 +1,20 @@
 function calculate() {
-    alert("calculating")
+    var errorMessage = invalidInputs();
+    if(errorMessage){
+        document.getElementById('result').innerHTML = errorMessage
+        return
+    }
+}
+
+function invalidInputs() {
+    var inputs = document.getElementsByTagName('input');
+    var errors = [];
+    for (var i = 0; i < inputs.length; i++) {
+        if (parseInt(inputs[i].value) >= parseInt(inputs[i].dataset["x"])) {
+            errors.push(inputs[i].dataset["x"]);
+        }
+    }
+    if (errors.length > 0) {   
+        return "The values for the following fields seem off: " + errors.join(", ") + ".<br>Please check your math."
+    }
 }
