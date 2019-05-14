7b735478b81bc77fb861f59e82f3dc949769af54
Yechiel K <contact@yechiel.me>
2019-05-13 20:48:21 -0400

    add link to codeland sourcecode

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,24 @@ Rails.application.routes.draw do
   
   root 'website#index'
 
+  # Static Pages
   get 'index', to: 'website#index'
-
   get 'contact', to: 'website#contact'
-
+  get 'magic', to: 'website#magic'
   get 'projects', to: 'website#projects'
 
+  # Internal Redirects
   get 'blog', to: 'website#blog'
-
   get 'resume', to: 'website#resume'
 
-  get 'magic', to: 'website#magic'
-
+  # External Redirects
   get 'zoom', to: redirect('https://pivotal.zoom.us/j/3845830623')
+  get 'codeland', to: redirect('https://github.com/achasveachas/codeland')
 
+  # Admin Pages
   get 'login', to: 'admin#login'
   get 'logout', to: 'admin#logout'
   post 'login', to: 'admin#new_session'
-
-  
-
   namespace :admin do
     root "visits#index"
     resources :visits, only: [:index, :show]
