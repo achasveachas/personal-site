72c211ece9c731b5456f00bb604473879f63e04e
Yechiel K <contact@yechiel.me>
2020-05-23 23:00:02 -0400

    update zoom link to vmaware

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'torahtech', to: 'website#torah_tech'
 
   # External Redirects
-  get 'zoom', to: redirect('https://pivotal.zoom.us/my/yechiel')
+  get 'zoom', to: redirect('https://vmware.zoom.us/my/yechiel')
   get 'codeland', to: redirect('https://github.com/achasveachas/codeland')
   get 'flatiron', to: redirect('https://github.com/achasveachas/flatiron-workshop')
   get 'firstyear', to: redirect('https://leanpub.com/firstyearincode')
