35d8c89b19a82dfe5efb5771d54913b9e46aff97
Yechiel K <contact@yechiel.me>
2021-07-21 22:05:50 -0400

    refactor the logic for recruiter pop-up

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,9 +3,9 @@ console.log("Thanks for stopping by!\nTo get in touch:\nWebsite: www.yechiel.me\
 if (window.location.search) {
 
     const urlParams = new URLSearchParams(window.location.search);
+    const company = urlParams.get("company")
 
-    if (urlParams.has("company")) {
-        const company = urlParams.get("company")
+    if (company && company !== "resume") {
         message = "Hello " + company + "!\n\nYou should totally hire Yechiel, I mean he's totally awesome!\n\nBut don't take my word for it, I'm just a simple browser, instead why don't you head over to the Projects page and take a look at some of the cool stuff he's made (or the resume if you want the boring version ;)\n\nSo waddaya say?"
         setTimeout(() => alert(message), 500)
     }
