aaf9efb8ffa9ed7cc00caeec60fb60f699423c23
Yechiel K <contact@yechiel.me>
2022-03-24 21:58:04 -0400

    change the dedman switch to reset automatically on visiting the admin page

--- app/controllers/admin/visits_controller.rb
+++ app/controllers/admin/visits_controller.rb
@@ -6,6 +6,8 @@ class Admin::VisitsController < ApplicationController
   def index
     @visits = Ahoy::Visit.joins(:events).group("ahoy_visits.id").limit(100).reverse_order
     @blogposts = BlogPost.all
+    @last_reset = Deadman.last_reset
+    Deadman.reset
   end
 
   def show
