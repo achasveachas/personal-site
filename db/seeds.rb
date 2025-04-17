f3e077fd9cf4d5fc6f9349563556139487b41b32
Yechiel K <contact@yechiel.me>
2025-04-16 22:20:07 -0400

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
     project.demo_url = Faker::Internet.url
     project.demo_username = "testuser"
     project.demo_password = "testpassword"
-    project.repo = Faker::Internet.url("github.com")
-    project.blog_post = Faker::Internet.url("blog.yechiel.me")
+    project.repo = Faker::Internet.url(hust: "github.com")
+    project.blog_post = Faker::Internet.url(host: "blog.yechiel.me")
     project.blog_title = Faker::Hipster.sentence
     project.save
 end
