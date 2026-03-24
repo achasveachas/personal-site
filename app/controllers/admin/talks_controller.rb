fa38c4ce43fbe11925a82abda205f83237fb7c37
Yechiel Kalmenson <contact@yechiel.me>
2026-03-24 16:45:44 -0400

    add admin functionality to add and edit talks

---
+++ app/controllers/admin/talks_controller.rb
@@ -0,0 +1,43 @@
+class Admin::TalksController < ApplicationController
+
+  before_action :require_admin
+  skip_before_action :track_ahoy_visit
+
+  def new
+    @talk = Talk.new
+  end
+
+  def edit
+    @talk = Talk.find_by(id: params[:id])
+  end
+
+  def create
+    @talk = Talk.new(talk_params)
+    if @talk.save
+      redirect_to talks_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @talk = Talk.find_by(id: params[:id])
+    if @talk.update(talk_params)
+      redirect_to talks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @talk = Talk.find_by(id: params[:id])
+    @talk.destroy
+    redirect_to talks_path
+  end
+
+  private
+
+  def talk_params
+    params.require(:talk).permit(:title, :description, :youtube_link, :blog_post_link, :conference_name, :talk_date, :picture_url)
+  end
+end
