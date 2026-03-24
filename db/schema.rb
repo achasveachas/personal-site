2cf9a35106eb0b0f93398c9cae65bb2e2429e517
Yechiel Kalmenson <contact@yechiel.me>
2026-03-24 16:41:50 -0400

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -2,88 +2,98 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_023913) do
-
+ActiveRecord::Schema[8.1].define(version: 2026_03_24_201924) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "ahoy_events", force: :cascade do |t|
-    t.bigint "visit_id"
     t.string "name"
     t.jsonb "properties"
     t.datetime "time"
+    t.bigint "visit_id"
     t.index ["name", "time"], name: "index_ahoy_events_on_name_and_time"
     t.index ["properties"], name: "index_ahoy_events_on_properties_jsonb_path_ops", opclass: :jsonb_path_ops, using: :gin
     t.index ["visit_id"], name: "index_ahoy_events_on_visit_id"
   end
 
   create_table "ahoy_visits", force: :cascade do |t|
-    t.string "visit_token"
-    t.string "visitor_token"
+    t.string "browser"
+    t.string "city"
+    t.string "company"
+    t.string "country"
+    t.string "device_type"
     t.string "ip"
-    t.text "user_agent"
+    t.text "landing_page"
+    t.string "os"
     t.text "referrer"
     t.string "referring_domain"
-    t.string "search_keyword"
-    t.text "landing_page"
-    t.string "browser"
-    t.string "os"
-    t.string "device_type"
-    t.string "country"
     t.string "region"
-    t.string "city"
-    t.string "utm_source"
-    t.string "utm_medium"
-    t.string "utm_term"
-    t.string "utm_content"
-    t.string "utm_campaign"
-    t.string "company"
+    t.string "search_keyword"
     t.datetime "started_at"
+    t.text "user_agent"
+    t.string "utm_campaign"
+    t.string "utm_content"
+    t.string "utm_medium"
+    t.string "utm_source"
+    t.string "utm_term"
+    t.string "visit_token"
+    t.string "visitor_token"
     t.index ["visit_token"], name: "index_ahoy_visits_on_visit_token", unique: true
   end
 
   create_table "blog_posts", force: :cascade do |t|
-    t.string "title"
-    t.string "url"
+    t.datetime "created_at", null: false
     t.string "slug"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "deadmen", force: :cascade do |t|
-    t.boolean "triggered", default: false
     t.datetime "created_at", null: false
+    t.boolean "triggered", default: false
     t.datetime "updated_at", null: false
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
+    t.string "blog_post"
+    t.string "blog_title"
+    t.datetime "created_at", null: false
+    t.string "demo_password"
     t.string "demo_url"
     t.string "demo_username"
-    t.string "demo_password"
+    t.string "description"
+    t.string "name"
     t.string "repo"
-    t.string "blog_post"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "blog_title"
   end
 
   create_table "session_logs", force: :cascade do |t|
     t.string "company"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "location"
     t.string "ip"
+    t.string "location"
     t.string "page_visited"
+    t.datetime "updated_at", null: false
   end
 
+  create_table "talks", force: :cascade do |t|
+    t.string "blog_post_link"
+    t.string "conference_name"
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "picture_url"
+    t.date "talk_date"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.string "youtube_link"
+  end
 end
