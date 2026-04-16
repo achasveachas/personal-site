5b1c9b778dbac304aee51179423bbbef075bb999
Yechiel Kalmenson <contact@yechiel.me>
2026-04-16 14:15:20 -0400

    make current role dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Current job info - update these when changing jobs
+  # Also remember to update the experience section in app/views/website/index.html.erb
+  def current_employer
+    "Rise8"
+  end
+  
+  def current_title
+    "Senior Software Engineer"
+  end
 end
